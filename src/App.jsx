f9fc4baee65fd660e609ba340cdc2b32cd9bc589
karanvir <kasg@simcorp.com>
2025-05-05 17:45:51 +0530

    Structuring done

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,7 @@
-import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data';
+import Header from './components/Header';
+import CoreConcept from './components/CoreConcept';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header(){
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props){
-  return (
-      <li>
-        <img src={props.image} alt={props.title}/>
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
-      </li>
-  );
-}
 
 function CoreConcept1({image, title, description}) {
   // const { image, title, description } = props; destructuring props
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import reactImg from '../assets/react-core-concepts.png';
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+//defference b/w export and export default
+export default function Header(){
+    const description = reactDescriptions[genRandomInt(2)];
+    return (
+      <header>
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    );
+  }
