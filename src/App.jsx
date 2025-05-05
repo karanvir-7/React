67c0380c7b72b694a06ace0363c7faf32270b416
karanvir <kasg@simcorp.com>
2025-05-05 17:32:36 +0530

    Props Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import reactImg from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data';
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
@@ -30,6 +32,17 @@ function CoreConcept(props){
   );
 }
 
+function CoreConcept1({image, title, description}) {
+  // const { image, title, description } = props; destructuring props
+  return (
+      <li>
+        <img src={image} alt={title}/>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+  );
+}
+
 function App() {
   return (
     <div>
@@ -39,10 +52,10 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
           <CoreConcept 
-            title = 'Components'
-            description = 'The core UI building block.'
-            image = {componentsImg}/>
-          <CoreConcept/>
+            title = {CORE_CONCEPTS[0].title}
+            description = {CORE_CONCEPTS[0].description}
+            image = {CORE_CONCEPTS[0].image}/>
+          <CoreConcept1 { ...CORE_CONCEPTS[1]}/>
           <CoreConcept/>
           <CoreConcept/>
         </ul>
