7a6c3c4099659405d98b083f1f96eea0a13cd9a4
karanvir <kasg@simcorp.com>
2025-05-16 12:26:54 +0530

    Outputting list dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -42,14 +42,14 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept {...conceptItem} />)}
+            {CORE_CONCEPTS.map((conceptItem,i) => <CoreConcept key={i} {...conceptItem} />)}
             {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
             <CoreConcept1 {...CORE_CONCEPTS[1]} />
-            <CoreConcept />
+            <CoreConcept /> 
             <CoreConcept /> */}
           </ul>
         </section>
