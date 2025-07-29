f60128fcc09d46c652b42d0fc12da9bff6cbf2c9
karanvir <kasg@simcorp.com>
2025-07-29 15:41:27 +0530

    Payload and state

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,32 @@
 import classes from './Counter.module.css';
+import React, { useState } from 'react';
 import { useSelector, connect, useDispatch} from 'react-redux';
 
 const Counter = () => {
   const dispatch = useDispatch(); 
+  const [isCounterVisible, setCounterVisible] = useState(true);
   const counter = useSelector((state) => state.counter);
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
   };
+
+  const increaseHandler= () =>{
+    dispatch({ type: 'increase', amount: 5 }); //action payload here is amount
+  }
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   };
-  const toggleCounterHandler = () => {};
 
+  const toggleCounterHandler = () => {
+    setCounterVisible((prevState) => !prevState);
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {isCounterVisible && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by Value</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,35 @@
 import { legacy_createStore } from 'redux';
 
-const counterReducer= (state = { counter: 0 },action) => {
+const initialState = { counter: 0 };
+//We always return a new state object in the reducer
+//We never mutate the state directly // if mutate we have unpredicable behavior and bugs
+//We always return a new state object in the reducer
+//It will always override the previous state
+//We can use the spread operator to copy the previous state
+
+const counterReducer= (state = initialState,action) => {
+
     if (action.type === 'increment') {
-        return { counter: state.counter + 1 };
+        return { 
+            ...state,
+            counter: state.counter + 1
+         }; 
     }   
-    if(action.type === 'decrement') {
-        return { counter: state.counter - 1 };
+
+    if (action.type == 'increase') {
+        return {
+            ...state,
+            counter: state.counter + action.amount
+        }
     }
+
+    if(action.type === 'decrement') {
+        return { 
+            ...state,
+            counter: state.counter - 1
+        };
+    }
+
     return state;
 }
 const store = legacy_createStore(counterReducer);
